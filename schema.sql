6bd567ec0219d09bf8ca792da3aafc9020d3f7fd
karlavdelgadof <karlavdelgadof@gmail.com>
2022-09-19 08:37:45 -0500

    Add database (animals table) structure

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(100),
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL
 );
